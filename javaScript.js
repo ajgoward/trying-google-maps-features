5fa39e0d4c5b731a7327cb0eed08f8f1cbe2835b
ajgoward <ajgoward@gmail.com>
2020-06-03 23:04:45 +0000

    managed to get more that one type of place to show on the map and can add keywords for children or dogs

--- javaScript.js
+++ javaScript.js
@@ -1,109 +1,203 @@
 let pos;
-let map;
-let bounds;
-let infoWindow;
-let currentInfoWindow;
-let service;
-let infoPane;
-function initMap() {
-    // Initialize variables
-    bounds = new google.maps.LatLngBounds();
-    infoWindow = new google.maps.InfoWindow;
-    currentInfoWindow = infoWindow;
-    /* TODO: Step 4A3: Add a generic sidebar */
+    let map;
+     let bounds;
+    let infoWindow;
+    let currentInfoWindow;
+    let service;
+    let infoPane;
+    
+    function initMap() {
+       bounds = new google.maps.LatLngBounds();
+      infoWindow = new google.maps.InfoWindow;
+      currentInfoWindow = infoWindow;
 
-    // Try HTML5 geolocation
-    if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-        pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-        };
-        map = new google.maps.Map(document.getElementById('map'), {
+      infoPane = document.getElementById('panel');
+
+       if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+          pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: pos,
+            zoom: 15
+          });
+          bounds.extend(pos);
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Location found.');
+          infoWindow.open(map);
+          map.setCenter(pos);
+            cafes(pos);
+        restaurants(pos);
+        parks(pos);
+        museums(pos);
+         }, () => {
+             handleLocationError(true, infoWindow);
+        });
+      } else {
+          handleLocationError(false, infoWindow);
+      }
+}
+
+         function handleLocationError(browserHasGeolocation, infoWindow) {
+              pos = {lat: -33.856, lng: 151.215};
+      map = new google.maps.Map(document.getElementById('map'), {
         center: pos,
         zoom: 15
+      });
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+        'Geolocation permissions denied. Using default location.' :
+        'Error: Your browser doesn\'t support geolocation.');
+      infoWindow.open(map);
+      currentInfoWindow = infoWindow;
+    
+       cafes(pos);
+        restaurants(pos);
+        parks(pos);
+        museums(pos);
+    }
+
+    function cafes(position) {
+      let request = {
+        location: position,
+        rankBy: google.maps.places.RankBy.DISTANCE,
+        type: 'cafe',
+        keyword: ''
+      };
+
+      service = new google.maps.places.PlacesService(map);
+      service.nearbySearch(request, nearbyCallback);
+    }
+
+    function restaurants(position) {
+      let request = {
+        location: position,
+        rankBy: google.maps.places.RankBy.DISTANCE,
+        type: 'restaurants',
+        keyword: 'dog-friendly'
+      };
+
+      service = new google.maps.places.PlacesService(map);
+      service.nearbySearch(request, nearbyCallback);
+    }
+
+        function museums(position) {
+      let request = {
+        location: position,
+        rankBy: google.maps.places.RankBy.DISTANCE,
+        type: 'museum',
+        keyword: ''
+      };
+
+      service = new google.maps.places.PlacesService(map);
+      service.nearbySearch(request, nearbyCallback);
+    }
+     function parks(position) {
+      let request = {
+        location: position,
+        rankBy: google.maps.places.RankBy.DISTANCE,
+        type: 'park',
+        keyword: 'dog-friendly'
+      };
+
+      service = new google.maps.places.PlacesService(map);
+      service.nearbySearch(request, nearbyCallback);
+    }
+
+
+    function nearbyCallback(results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        createMarkers(results);
+      }
+    }
+    
+
+    function createMarkers(places) {
+      places.forEach(place => {
+        let marker = new google.maps.Marker({
+          position: place.geometry.location,
+          map: map,
+          title: place.name,
+        
         });
-        bounds.extend(pos);
+        
 
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
-        infoWindow.open(map);
-        map.setCenter(pos);
+         google.maps.event.addListener(marker, 'click', () => {
+          let request = {
+            placeId: place.place_id,
+            fields: ['name', 'formatted_address', 'geometry', 'rating',
+              'website', 'photos']
+          };
 
-        /* TODO: Step 3B2, Call the Places Nearby Search */
-        getNearbyPlaces(pos);
-    }, () => {
-        // Browser supports geolocation, but user has denied permission
-        handleLocationError(true, infoWindow);
-    });
-    } else {
-    // Browser doesn't support geolocation
-    handleLocationError(false, infoWindow);
+          service.getDetails(request, (placeResult, status) => {
+            showDetails(placeResult, marker, status)
+          });
+        });
+
+         bounds.extend(place.geometry.location);
+      });
+
+       map.fitBounds(srictbounds);
     }
-}
 
-// Handle a geolocation error
-function handleLocationError(browserHasGeolocation, infoWindow) {
-    // Set default location to Sydney, Australia
-    pos = {lat: -33.856, lng: 151.215};
-    map = new google.maps.Map(document.getElementById('map'), {
-    center: pos,
-    zoom: 15
-    });
-
-    // Display an InfoWindow at the map center
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-    'Geolocation permissions denied. Using default location.' :
-    'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
-    currentInfoWindow = infoWindow;
-
-    /* TODO: Step 3B3, Call the Places Nearby Search */
-    getNearbyPlaces(pos);
-}
-function getNearbyPlaces(position) {
-   if (document.getElementById("differentPlaces").value == "1"){
-        let request = {
-    location: position,
-    rankBy: google.maps.places.RankBy.DISTANCE,
-    type: 'cafe',
-    keyword: 'dog-friendly'
-    } (document.getElementById("differentPlaces").value == "2"){
-        let request = {
-    location: position,
-    rankBy: google.maps.places.RankBy.DISTANCE,
-    type: 'restaurants',
-    keyword: 'dog-friendly'
-    }};
-
-    service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, nearbyCallback);
-}
-
-// Handle the results (up to 20) of the Nearby Search
-function nearbyCallback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-    createMarkers(results);
+    function showDetails(placeResult, marker, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        let placeInfowindow = new google.maps.InfoWindow();
+        let rating = "None";
+        if (placeResult.rating) rating = placeResult.rating;
+        placeInfowindow.setContent('<div><strong>' + placeResult.name +
+          '</strong><br>' + 'Rating: ' + rating + '</div>');
+        placeInfowindow.open(marker.map, marker);
+        currentInfoWindow.close();
+        currentInfoWindow = placeInfowindow;
+        showPanel(placeResult);
+      } else {
+        console.log('showDetails failed: ' + status);
+      }
     }
-}
-function createMarkers(places) {
-    places.forEach(place => {
-    let marker = new google.maps.Marker({
-        position: place.geometry.location,
-        map: map,
-        title: place.name,
-    
-    });/* TODO: Step 4B: Add click listeners to the markers */
 
-    // Adjust the map bounds to include the location of this marker
-    bounds.extend(place.geometry.location);
-    });
-    /* Once all the markers have been placed, adjust the bounds of the map to
-    * show all the markers within the visible area. */
-    map.fitBounds(bounds);
-}
+       function showPanel(placeResult) {
+            if (infoPane.classList.contains("open")) {
+        infoPane.classList.remove("open");
+      }
+       while (infoPane.lastChild) {
+        infoPane.removeChild(infoPane.lastChild);
+      }
+      if (placeResult.photos) {
+        let firstPhoto = placeResult.photos[0];
+        let photo = document.createElement('img');
+        photo.classList.add('hero');
+        photo.src = firstPhoto.getUrl();
+        infoPane.appendChild(photo);
+      }
+       let name = document.createElement('h1');
+      name.classList.add('place');
+      name.textContent = placeResult.name;
+      infoPane.appendChild(name);
+      if (placeResult.rating) {
+        let rating = document.createElement('p');
+        rating.classList.add('details');
+        rating.textContent = `Rating: ${placeResult.rating} \u272e`;
+        infoPane.appendChild(rating);
+      }
+      let address = document.createElement('p');
+      address.classList.add('details');
+      address.textContent = placeResult.formatted_address;
+      infoPane.appendChild(address);
+      if (placeResult.website) {
+        let websitePara = document.createElement('p');
+        let websiteLink = document.createElement('a');
+        let websiteUrl = document.createTextNode(placeResult.website);
+        websiteLink.appendChild(websiteUrl);
+        websiteLink.title = placeResult.website;
+        websiteLink.href = placeResult.website;
+        websitePara.appendChild(websiteLink);
+        infoPane.appendChild(websitePara);
+      }
+       infoPane.classList.add("open");
+    }
 
-function changePlaces (getNearbyPlaces){
-    
-}}
